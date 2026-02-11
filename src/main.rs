7484c2d68efc4527bd2f8869cbcafcf783123af8
rit-steph-y <229365513+rit-steph-y@users.noreply.github.com>
2026-02-11 13:57:44 -0500

    silly nonesense: taiko

--- src/main.rs
+++ src/main.rs
@@ -66,10 +66,6 @@ fn spawn_notes(mut commands: Commands) {
         spawn_note(&mut commands, 1000 * i);
         spawn_note(&mut commands, 1000 * i + 200);
     }
-    // spawn_note(&mut commands, 1000);
-    // // spawn_note(&mut commands, 1500);
-    // // spawn_note(&mut commands, 1800);
-    // spawn_note(&mut commands, 3000);
 }
 
 const DON_BUTTONS: &[KeyCode] = &[KeyCode::KeyF, KeyCode::KeyJ];
@@ -86,7 +82,7 @@ fn handle_key_inputs(mut commands: Commands, keyboard_input: Res<ButtonInput<Key
 }
 
 const UNITS_PER_SECOND: f32 = -500.;
-const HIT_POSITION: Vec2 = Vec2::new(600., 100.);
+const HIT_POSITION: Vec2 = Vec2::new(500., 100.);
 
 fn update_note_transform(mut query: Query<(&mut Transform, &TimeUntilClick)>) {
     for (mut transform, time_until_click) in &mut query {
@@ -103,36 +99,6 @@ fn is_key_group_hit(keyboard_input: &Res<ButtonInput<KeyCode>>, keys: &[KeyCode]
         .count()
         > 0
 }
-
-// These constants are defined in `Transform` units.
-// Using the default 2D camera they correspond 1:1 with screen pixels.
-// const PADDLE_SIZE: Vec2 = Vec2::new(120.0, 20.0);
-// const GAP_BETWEEN_PADDLE_AND_FLOOR: f32 = 60.0;
-// const PADDLE_SPEED: f32 = 500.0;
-// // How close can the paddle get to the wall
-// const PADDLE_PADDING: f32 = 10.0;
-
-// We set the z-value of the ball to 1 so it renders on top in the case of overlapping sprites.
-// const BALL_STARTING_POSITION: Vec3 = Vec3::new(0.0, -50.0, 1.0);
-// const BALL_DIAMETER: f32 = 30.;
-// const BALL_SPEED: f32 = 400.0;
-// const INITIAL_BALL_DIRECTION: Vec2 = Vec2::new(0.5, -0.5);
-
-// const WALL_THICKNESS: f32 = 10.0;
-// // x coordinates
-// const LEFT_WALL: f32 = -450.;
-// const RIGHT_WALL: f32 = 450.;
-// // y coordinates
-// const BOTTOM_WALL: f32 = -300.;
-// const TOP_WALL: f32 = 300.;
-
-// const BRICK_SIZE: Vec2 = Vec2::new(100., 30.);
-// // These values are exact
-// const GAP_BETWEEN_PADDLE_AND_BRICKS: f32 = 270.0;
-// const GAP_BETWEEN_BRICKS: f32 = 5.0;
-// // These values are lower bounds, as the number of bricks is computed
-// const GAP_BETWEEN_BRICKS_AND_CEILING: f32 = 20.0;
-// const GAP_BETWEEN_BRICKS_AND_SIDES: f32 = 20.0;
 
 const SCOREBOARD_FONT_SIZE: f32 = 33.0;
 const SCOREBOARD_TEXT_PADDING: Val = Val::Px(5.0);
@@ -166,43 +132,15 @@ fn main() {
             Update,
             (update_timed, (update_note_transform, mark_pressable), handle_key_inputs, free_timed).chain(),
         )
-        .add_observer(play_collision_sound)
+        // .add_observer(play_collision_sound)
         .run();
 }
 
-#[derive(Component)]
-struct Paddle;
 
-#[derive(Component)]
-struct Ball;
-
-#[derive(Component, Deref, DerefMut)]
-struct Velocity(Vec2);
-
-#[derive(Event)]
-struct BallCollided;
-
-#[derive(Component)]
-struct Brick;
 
 #[derive(Resource, Deref)]
 struct CollisionSound(Handle<AudioSource>);
 
-// Default must be implemented to define this as a required component for the Wall component below
-#[derive(Component, Default)]
-struct Collider;
-
-// This is a collection of the components that define a "Wall" in our game
-#[derive(Component)]
-#[require(Sprite, Transform, Collider)]
-struct Wall;
-
-#[derive(Component, Default)]
-struct PhysicsTransform(Vec2);
-
-#[derive(Component)]
-#[require(PhysicsTransform)]
-struct PhysicsTransformInterpolate(Option<(Vec2, f64)>);
 // This resource tracks the game's score
 #[derive(Resource, Deref, DerefMut)]
 struct Score(usize);
@@ -213,8 +151,6 @@ struct ScoreboardUi;
 // Add the game's entities to our world
 fn setup(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
     // Camera
@@ -252,34 +188,10 @@ fn setup(
     ));
 }
 
-fn update_visual_transform(
-    mut moving_query: Query<(
-        &mut Transform,
-        &PhysicsTransformInterpolate,
-        &PhysicsTransform,
-    )>,
-    time: Res<Time>,
-) {
-    const FIXED_UPDATE_INTERVAL: f64 = 1. / 64.;
-    for (mut transform, interpolate, current) in &mut moving_query {
-        let current_pos = current.0;
-        let (last_pos, last_t) = interpolate
-            .0
-            .unwrap_or((current_pos, time.elapsed_secs_f64()));
-
-        let lerp_i = (time.elapsed_secs_f64() - last_t) / FIXED_UPDATE_INTERVAL;
-
-        let interp_pos = last_pos.lerp(current_pos, lerp_i as f32);
-
-        transform.translation.x = interp_pos.x;
-        transform.translation.y = interp_pos.y;
-    }
-}
-
-fn play_collision_sound(
-    _collided: On<BallCollided>,
-    mut commands: Commands,
-    sound: Res<CollisionSound>,
-) {
-    commands.spawn((AudioPlayer(sound.clone()), PlaybackSettings::DESPAWN));
-}
+// fn play_collision_sound(
+//     _collided: On<BallCollided>,
+//     mut commands: Commands,
+//     sound: Res<CollisionSound>,
+// ) {
+//     commands.spawn((AudioPlayer(sound.clone()), PlaybackSettings::DESPAWN));
+// }
